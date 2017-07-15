557dde239a93117c48fbae495ae168333262cc94
Meenu  Agarwal <mee.agarwal97@gmail.com>
2017-07-15 13:14:58 +0530

    Update binarySearch.py

--- PYTHON/binarySearch.py
+++ PYTHON/binarySearch.py
@@ -1,3 +1,7 @@
+''' Binary Search: Search a sorted array by repeatedly dividing the search interval in half. 
+Begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, 
+narrow the interval to the lower half. Otherwise narrow it to the upper half. 
+Repeatedly check until the value is found or the interval is empty.'''
 def BinarySearch(arr,num):
 	first,last = 0, len(arr)-1
 	found = False
