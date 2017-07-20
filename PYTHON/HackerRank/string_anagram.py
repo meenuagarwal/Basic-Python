624cd8ccbefbbcc63509e962227b067774b07411
Meenu  Agarwal <mee.agarwal97@gmail.com>
2017-07-20 16:29:28 +0530

    Update string_anagram.py

--- PYTHON/HackerRank/string_anagram.py
+++ PYTHON/HackerRank/string_anagram.py
@@ -1,3 +1,6 @@
+''' Given two strings a and b that may or may not be of the same length, 
+    determine the minimum number of character deletions required to make a and b anagrams. '''
+
 def number_needed(a, b):
     map_a = buildmap(a)
     map_b = buildmap(b)
